167f995bfbdbd952a36ea871745a75d844608551
Cameron Cook <Cameron.cook0310@gmail.com>
2024-12-05 14:45:12 -0600

    Finished pt1, moved pt1, added pt2

---
+++ Day1/historian_hysteria_pt1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    # The input for this puzzle is a string with two lists (columns) of numbers seperated by whitespace
+    puzzle_input = open("input.txt", "r")
+
+    left_list = []
+    right_list = []
+
+    dist_sum = 0
+    
+    # Loops through each line of the input file, splitting the numbers of that line at the whitespace and then adding each
+    # number to its respective list
+    for line in puzzle_input:
+        line = line.strip()
+        nums = line.split()
+        left_list.append(int(nums[0]))
+        right_list.append(int(nums[1]))
+
+
+    left_list.sort()
+    right_list.sort()
+
+    for i in range(len(left_list)):
+        dist = abs(left_list[i] - right_list[i])
+        dist_sum += dist
+       
+    print(dist_sum)
+
+    puzzle_input.close()
+
+
+if __name__ == "__main__":
+    main()
