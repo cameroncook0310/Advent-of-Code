9bd0d93cf02efe56d39f173446f802faa039655e
Cameron Cook <Cameron.cook0310@gmail.com>
2024-12-18 14:00:29 -0600

    Complete day4-2.py

--- Day 4/day4-2.py
+++ Day 4/day4-2.py
@@ -6,25 +6,24 @@ def check_valid_idx(matrix, i, j):
     else:
         return False 
 
-def find_xmas(matrix, curr_i, curr_j):
-    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-    xmas_count = 0
-    target = "XMAS"
-    for dir in directions:
-        check_str = ""
-        for i in range(len(target)):
-            new_i = curr_i + dir[0] * i
-            new_j = curr_j + dir[1] * i
-            if check_valid_idx(matrix, new_i, new_j) == False:
-                break
-            check_str += matrix[new_i][new_j]
-            if check_str not in target:
-                break
-        
-        if check_str == target:
-            xmas_count += 1
+def is_xmas_cross(matrix, curr_i, curr_j):
+    directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
 
-    return xmas_count
+    target_set = set(["M", "A", "S"])
+    left_diag_set = set(["A"])
+    right_diag_set = set(["A"])
+
+    for dir in directions:
+        new_i = curr_i + dir[0]
+        new_j = curr_j + dir[1]
+        if check_valid_idx(matrix, new_i, new_j) == False:
+            return False
+        if dir in [(-1, -1), (1, 1)]:
+            left_diag_set.add(matrix[new_i][new_j])
+        else:
+            right_diag_set.add(matrix[new_i][new_j])
+
+    return left_diag_set == target_set and right_diag_set == target_set
 
 def main():
     input = open("./day 4/input.txt", "r")
@@ -38,9 +37,8 @@ def main():
 
     for i in range(len(input_matrix)):
         for j in range(len(input_matrix[i])): 
-            if input_matrix[i][j] == 'X':
-                #xmas_count += find_xmas(input_matrix, i, j, "",set())
-                xmas_count += find_xmas(input_matrix, i, j)
+            if input_matrix[i][j] == 'A' and is_xmas_cross(input_matrix, i, j):
+                xmas_count += 1
     
     print(xmas_count)
 
