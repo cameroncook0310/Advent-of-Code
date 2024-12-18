e8dd0487cd468a46b644d1cd56bb3c99cdb59134
Cameron Cook <Cameron.cook0310@gmail.com>
2024-12-18 10:56:09 -0600

    Add day3-2.py

---
+++ Day 3/day3-2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import re
+
+def mul(num1, num2):
+    return int(num1) * int(num2)
+
+def main():
+    input = open("./day 3/input.txt", "r")
+    input_text = input.read()
+
+    valid_muls = re.findall("don't\(\)|do\(\)|mul\(\d+,\d+\)",input_text)
+
+    muls_sum = 0
+
+    for curr_mul in valid_muls:
+        muls_sum += eval(curr_mul)
+    
+    print(muls_sum)
+
+if __name__ == "__main__":
+    main()
